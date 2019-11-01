f4c82f930dacc79f83c5b2dd298f1efa57aedb10
bruno <brunocpessoal@gmail.com>
2019-11-01 16:37:09 +0000

    implemented websocket communication with backend and like function

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Image, StyleSheet,  TouchableOpacity, FlatList, Text } from 'react-native';
 
+import io from 'socket.io-client';
+
 import api from '../services/api';
 import camera from '../assets/camera.png';
 import more from '../assets/more.png';
@@ -22,11 +24,31 @@ export default class Feed extends Component {
   };
 
   async componentDidMount() {
-    // this.registerToSocket();
+    this.registerToSocket();
 
     const response = await api.get('posts');
 
     this.setState({ feed: response.data });
+  }
+
+  registerToSocket = () => {
+    const socket = io('http://192.168.0.103:3333');
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post  
+        )
+      });
+    })
+  }
+
+  handleLike = id => {
+    api.post(`posts/${id}/like`);
   }
 
   render() {
@@ -50,7 +72,7 @@ export default class Feed extends Component {
 
               <View style={styles.feedItemFooter}>
                 <View style={styles.actions}>
-                  <TouchableOpacity style={styles.action} onPress={() => {}}>
+                  <TouchableOpacity style={styles.action} onPress={() => this.handleLike(item._id)}>
                     <Image source={like} />
                   </TouchableOpacity>
                   <TouchableOpacity style={styles.action} onPress={() => {}}>
